86c7fbf01855799bcc22ac96381c2cc06c6a4899
ksp5389 <ksp5389@psu.edu>
2024-09-16 21:52:22 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "ksp5389";
+    char STUDENT_ID[] = "976315277";
 
     return 0 ;
 }
